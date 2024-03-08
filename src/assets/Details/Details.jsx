60b4251230d3257c2b8896b523cf1a3a89412600
SteevenSF01 <salgadosteeven@gmail.com>
2024-03-08 16:40:50 +0100

    ajout des borders dans les details

--- src/assets/Details/Details.jsx
+++ src/assets/Details/Details.jsx
@@ -7,17 +7,19 @@ export default function Details() {
   const { idPays = 0 } = useParams();
 
   const details = data[idPays];
-
+  console.log(details.borders);
 
   return (
     <>
-      <section className={`${theme ? 'text-gray-600' : 'text-white'} h-fit`}>
+      <section className={`${theme ? "text-gray-600" : "text-white"} h-fit`}>
         <Link to="/">
           <button className="text-white border-2 border-black py-2 px-5 bg-[#2B3743ff] rounded-lg mt-5 ms-5">
             {"<"} Back
           </button>
         </Link>
-        <div className={`h-[80%] flex flex-col items-center justify-between py-5 `}>
+        <div
+          className={`h-[80%] flex flex-col items-center justify-between py-5 `}
+        >
           <div className="w-[80%] h-[250px] border-[1px] border-gray-800 shadow-[0px_0px_8px_1px_#2d3748]">
             <img
               src={details.flags.png}
@@ -27,9 +29,7 @@ export default function Details() {
           </div>
           <div className="mt-5 w-[80%] h-fit shadow-[0px_0px_10px_5px_#2d3748] rounded-xl p-4">
             <article className="w-full h-full flex flex-col gap-5">
-              <h1 className="text-xl font-semibold">
-                {details.name.common}{" "}
-              </h1>
+              <h1 className="text-xl font-semibold">{details.name.common} </h1>
               <div className="flex justify-between">
                 <div className="px-2">
                   <p className="flex flex-wrap">
@@ -58,14 +58,14 @@ export default function Details() {
                     Sub Region :
                     <span className="ps-1 text-gray-400">
                       {" "}
-                      {details.subregion ? details.subregion : 'Undefined'}
+                      {details.subregion ? details.subregion : "Undefined"}
                     </span>
                   </p>
                   <p className="flex flex-wrap">
                     Capital :
                     <span className="ps-1 text-gray-400">
                       {" "}
-                      {details.capital ? details.capital : 'Undefined'}
+                      {details.capital ? details.capital : "Undefined"}
                     </span>
                   </p>
                 </div>
@@ -92,7 +92,14 @@ export default function Details() {
                   </p>
                 </div>
               </div>
+              <div className="flex items-center gap-5">
               <h1 className="my-2">Border Countries:</h1>
+                {details.borders && details.borders.length > 0
+                  ? details.borders.map((element, i) => (
+                      <button key={i}>{element}</button>
+                    ))
+                  : "Undefined"}
+              </div>
             </article>
           </div>
         </div>
